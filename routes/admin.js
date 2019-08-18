ceaba717125d919b00a17269ade27ae2b1a5486b
Saleh Suboh <subohsaleh@gmail.com>
2019-08-18 08:04:54 -0400

    Added more features for data saving without db

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => { 
-    console.log(req.body);
+router.post('/add-product', (req, res, next) => { 
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
